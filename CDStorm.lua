751eba46ff5551736301101ba129a27052b1f719
zuqorl <alextheo482@gmail.com>
2025-03-04 18:00:52 +0100

    Add files via upload

--- CDStorm.lua
+++ CDStorm.lua
@@ -115,10 +115,32 @@ tab2.newToggle("Farm VW points", "Will race until you stop it.You must take a ca
 local tab3 = DrRayLibrary.newTab("Farming", "ImageIdHe")
 
 -- Add elements to the second tab
-tab3.newToggle("Farm Money", "Will drive until you stop it.You must take a car!", false, function(toggleState)
-    if toggleState then
-        print("On")
-    else
-        print("Off")
-    end
-end)
+tab3.newToggle("Farm Money", "Will drive until you stop it.You must take a car!(Bêta version)", false, function(state)
+  getfenv().auto = (state and true or false)
+  wait(1)
+  workspace.Gravity = getfenv().grav
+  while getfenv().auto do
+    task.wait()
+  local chr = game.Players.LocalPlayer.Character
+  local car = chr.Humanoid.SeatPart.Parent.Parent
+    if not workspace:FindFirstChild("justapart") then
+      local new = Instance.new("Part",workspace)
+      new.Name = "justapart"
+      new.Size = Vector3.new(10000,20,10000)
+      new.Anchored = true
+      new.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position+Vector3.new(0,1000,0)
+  end
+  car:PivotTo(workspace:FindFirstChild("justapart").CFrame*CFrame.new(0,7,1000))
+  local pos = workspace:FindFirstChild("justapart").CFrame*CFrame.new(0,7,-1000)
+  repeat task.wait()
+    local speed =  getfenv().speed or 300
+    local accel = 300
+    workspace.Gravity = 500
+            car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+    car:PivotTo(CFrame.new(car.PrimaryPart.Position,Vector3.new(pos.X,car.PrimaryPart.Position.Y,pos.Z)))
+            car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+    car.PrimaryPart.AssemblyLinearVelocity = car.PrimaryPart.CFrame.LookVector*speed
+    car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+  until game.Players.LocalPlayer:DistanceFromCharacter(Vector3.new(pos.X,pos.Y,pos.Z)) < 200 or getfenv().test == false
+  end
+  end)
